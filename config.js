ff65d248f9f76f66046d34e9988a04c39b2d8bf2
aeshalathyf <aesha.lathyf@gmail.com>
2026-04-30 00:24:00 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-const SUPABASE_URL = 'https://inxzqedngfbylpuhplih.supabase.co';        // e.g. https://abcdefgh.supabase.co
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlueHpxZWRuZ2ZieWxwdWhwbGloIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzczNTY1MDYsImV4cCI6MjA5MjkzMjUwNn0.FpCvYT-CzCjXr4EoFT3tCreGv7fa2B5P_0yr_O3h6GM';  // long string starting with eyJ...
+const SUPABASE_URL = 'https://inxzqedngfbylpuhplih.supabase.co'; 
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlueHpxZWRuZ2ZieWxwdWhwbGloIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzczNTY1MDYsImV4cCI6MjA5MjkzMjUwNn0.FpCvYT-CzCjXr4EoFT3tCreGv7fa2B5P_0yr_O3h6GM';  
 
 // Don't change anything below this line unless you know what you're doing
 window.SUPABASE_CONFIG = { url: https://inxzqedngfbylpuhplih.supabase.co, key: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlueHpxZWRuZ2ZieWxwdWhwbGloIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzczNTY1MDYsImV4cCI6MjA5MjkzMjUwNn0.FpCvYT-CzCjXr4EoFT3tCreGv7fa2B5P_0yr_O3h6GM};
