f05b79c68ad76282562c08ccad7c7b2566a0af66
aeshalathyf <aesha.lathyf@gmail.com>
2026-04-30 00:36:07 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 const SUPABASE_URL = 'https://inxzqedngfbylpuhplih.supabase.co';
-   const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6...';
+   const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlueHpxZWRuZ2ZieWxwdWhwbGloIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzczNTY1MDYsImV4cCI6MjA5MjkzMjUwNn0.FpCvYT-CzCjXr4EoFT3tCreGv7fa2B5P_0yr_O3h6GM';
